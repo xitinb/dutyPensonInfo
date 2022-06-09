6ee211c4176bd8d552425fc9ff095e1eccf68f98
zhengxb <1072558352@qq.com>
2022-06-09 10:43:43 +0800

    hello world

---
+++ src/main/java/cn/gzsendi/huangyj/listener/test.java
@@ -0,0 +1,17 @@
+package cn.gzsendi.huangyj.listener;
+
+/**
+ * @author: gfk
+ * @create: 2022/6/9
+ * @Description:
+ * @FileName: test
+ * @自定义内容：
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
+
+
